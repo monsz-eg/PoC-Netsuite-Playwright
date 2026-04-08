45d5d6d5f0ca5bd0f5af0c87d28b806901a4d76d
monsz <monsz@eg.dk>
2026-04-08 06:51:42 -0700

    Update baseURL in Playwright configuration for Netsuite testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://5177942-sb3.app.netsuite.com/app/center/card.nl?sc=-29&whence=',
 
     trace: 'on-first-retry',
   },
