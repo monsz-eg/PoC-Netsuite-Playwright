b85527a91f64b9ee9effa3ae8b7d75350bab3c5d
monsz <monsz@eg.dk>
2026-04-22 06:15:51 -0700

    fix(invoiceTest): update invoice creation test to use dynamic memo generation and correct customer reference

--- tests/invoice/invoice.spec.ts
+++ tests/invoice/invoice.spec.ts
@@ -1,43 +1,36 @@
-import { INVOICE_DATA } from "../constants/invoiceData";
-import { ROLES } from "../constants/roles";
-import { test } from "../fixtures/baseFixture";
-import { InvoiceRecord } from "../pages/InvoiceRecord";
-import { today } from "../utils/dateUtils";
-import { generatePoNumber } from "../utils/nameGenerators";
+import { INVOICE_DATA } from '../constants/invoiceData';
+import { ROLES } from '../constants/roles';
+import { test } from '../fixtures/baseFixture';
+import { InvoiceRecord } from '../pages/InvoiceRecord';
+import { today } from '../utils/dateUtils';
+import { generateMemo, generatePoNumber } from '../utils/nameGenerators';
 
-test.use({ isolatedStorageState: "auth/nstest2.json" });
+test.use({ isolatedStorageState: 'auth/nstest2.json' });
 
-test("billing responsible can create a new Invoice @smoke", async ({
-  isolatedPage,
-}) => {
+test('billing responsible can create a new Invoice @smoke', async ({ isolatedPage }) => {
   // Arrange
   const invoiceRecord = new InvoiceRecord(isolatedPage);
   const poNumber = generatePoNumber();
+  const memo = generateMemo();
 
   await invoiceRecord.switchRole(ROLES.egBillingResponsible);
   await invoiceRecord.navigateToInvoice();
 
   // Act
-  await invoiceRecord.setCustomer(INVOICE_DATA.customerId);
-  await invoiceRecord.setSubsidiary(INVOICE_DATA.subsidiaryId);
+  await invoiceRecord.setCustomer(INVOICE_DATA.customerText);
+  await invoiceRecord.verifySubsidiaryPrepopulated(INVOICE_DATA.subsidiaryId);
   await invoiceRecord.setAccount(INVOICE_DATA.accountId);
-  await invoiceRecord.setMemo(INVOICE_DATA.memo);
+  await invoiceRecord.setMemo(memo);
   await invoiceRecord.setPONumber(poNumber);
   await invoiceRecord.setOrderedBy(INVOICE_DATA.orderedById);
   await invoiceRecord.addLineItem(INVOICE_DATA.lineItemText);
   await invoiceRecord.setLineItemDescription(INVOICE_DATA.lineItemDescription);
   await invoiceRecord.setLineItemQuantity(INVOICE_DATA.lineItemQuantity);
   await invoiceRecord.setLineItemRate(INVOICE_DATA.lineItemRate);
-  await invoiceRecord.setLineItemMainProduct(
-    INVOICE_DATA.lineItemMainProductId,
-  );
+  await invoiceRecord.setLineItemMainProduct(INVOICE_DATA.lineItemMainProductId);
   await invoiceRecord.setLineItemSubProduct(INVOICE_DATA.lineItemSubProductId);
-  await invoiceRecord.setLineItemProductItem(
-    INVOICE_DATA.lineItemProductItemId,
-  );
-  await invoiceRecord.setLineItemRevenueCategory(
-    INVOICE_DATA.lineItemRevenueCategoryId,
-  );
+  await invoiceRecord.setLineItemProductItem(INVOICE_DATA.lineItemProductItemId);
+  await invoiceRecord.setLineItemRevenueCategory(INVOICE_DATA.lineItemRevenueCategoryId);
   await invoiceRecord.setLineItemDepartment(INVOICE_DATA.lineItemDepartmentId);
   await invoiceRecord.setLineItemRevStartDate(today());
   await invoiceRecord.setLineItemRevEndDate(today());
@@ -50,7 +43,7 @@ test("billing responsible can create a new Invoice @smoke", async ({
   await invoiceRecord.verifyBillToCustomer(INVOICE_DATA.customerText);
   await invoiceRecord.verifySubsidiary(INVOICE_DATA.subsidiaryText);
   await invoiceRecord.verifyCurrency(INVOICE_DATA.currencyText);
-  await invoiceRecord.verifyMemo(INVOICE_DATA.memo);
+  await invoiceRecord.verifyMemo(memo);
   await invoiceRecord.verifyPONumber(poNumber);
   await invoiceRecord.verifyOrderedBy(INVOICE_DATA.orderedByText);
   await invoiceRecord.verifyTranDate();
