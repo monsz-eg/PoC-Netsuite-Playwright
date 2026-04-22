60fd9ed877782280f25f2c2ec4d5a6c34ea3e501
monsz <monsz@eg.dk>
2026-04-22 06:13:03 -0700

    refactor(invoiceData): replace hardcoded values with dynamic references from lookups

--- tests/constants/invoiceData.ts
+++ tests/constants/invoiceData.ts
@@ -1,26 +1,43 @@
+import {
+  ACCOUNTS,
+  CURRENCIES,
+  CUSTOMERS,
+  DEPARTMENTS,
+  ITEMS,
+  PRODUCTS,
+  REVENUE_CATEGORIES,
+  SUBSIDIARIES,
+} from './lookups';
+
+const LINE_ITEM_DEFAULTS = {
+  description: 'test',
+  quantity: '1',
+  rate: '100',
+} as const;
+
 export const INVOICE_DATA = {
-  customerId: "37214", // NS internal ID for "Priess A/S"
-  customerText: "046141 Priess A/S",
-  subsidiaryId: "15", // NS internal ID for "EGDK"
-  accountId: "297", // NS internal ID for "Trade receivables" (1221000)
-  subsidiaryText: "EGDK",
-  currencyText: "DKK",
-  memo: "test memo text",
-  orderedById: "3013",
-  orderedByText: "076876 Sears Manufacturing Co.",
-  lineItemId: "9180",
-  lineItemText: ".[Hours] Consultancy services - T&M Training",
-  lineItemDescription: "test",
-  lineItemQuantity: "2",
-  lineItemRate: "100",
-  lineItemRevenueCategoryId: "805",
-  lineItemRevenueCategoryText: "Perpetual Licenses",
-  lineItemDepartmentId: "5702",
-  lineItemDepartmentText: "00003 NetSuite",
-  lineItemMainProductId: "103",
-  lineItemMainProductText: "Dynaway - EAM",
-  lineItemSubProductId: "105",
-  lineItemSubProductText: "Dynaway F&O - EAM",
-  lineItemProductItemId: "119",
-  lineItemProductItemText: "Advanced Enterprise Asset Management - EAM",
+  customerId: CUSTOMERS.priessAS.id,
+  customerText: CUSTOMERS.priessAS.displayName,
+  subsidiaryId: CUSTOMERS.priessAS.subsidiary,
+  subsidiaryText: SUBSIDIARIES.egdk.displayName,
+  accountId: ACCOUNTS.tradeReceivables.id,
+  currencyText: CURRENCIES.dkk.displayName,
+  orderedById: CUSTOMERS.searsManufacturingCo.id,
+  orderedByText: CUSTOMERS.searsManufacturingCo.displayName,
+  lineItemText: ITEMS.hoursConsultancyServicesTAndMTraining.displayName,
+  lineItemDescription: LINE_ITEM_DEFAULTS.description,
+  lineItemQuantity: LINE_ITEM_DEFAULTS.quantity,
+  lineItemRate: LINE_ITEM_DEFAULTS.rate,
+  lineItemRevenueCategoryId: REVENUE_CATEGORIES.perpetualLicenses.id,
+  lineItemRevenueCategoryText: REVENUE_CATEGORIES.perpetualLicenses.displayName,
+  lineItemDepartmentId: DEPARTMENTS.netSuite.id,
+  lineItemDepartmentText: DEPARTMENTS.netSuite.displayName,
+  lineItemMainProductId: PRODUCTS.dynawayEAM.id,
+  lineItemMainProductText: PRODUCTS.dynawayEAM.displayName,
+  lineItemSubProductId: PRODUCTS.dynawayEAM.dynawayFoEAM.id,
+  lineItemSubProductText: PRODUCTS.dynawayEAM.dynawayFoEAM.displayName,
+  lineItemProductItemId:
+    PRODUCTS.dynawayEAM.dynawayFoEAM.items.advancedEnterpriseAssetManagementEAM.id,
+  lineItemProductItemText:
+    PRODUCTS.dynawayEAM.dynawayFoEAM.items.advancedEnterpriseAssetManagementEAM.displayName,
 };
