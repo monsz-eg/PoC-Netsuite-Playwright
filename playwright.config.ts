fc58603c80667c7de3016219965bf5a1926ce6c4
MalgoDobosz <madob@eg.dk>
2026-04-27 01:06:46 -0700

    fix(config): increase test timeout to improve stability of test execution

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ loadEnv();
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 120000,
+  timeout: 240000,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
