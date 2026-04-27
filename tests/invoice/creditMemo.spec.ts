c6b146bb8d497678080d099d58e019e75b11f05c
monsz <monsz@eg.dk>
2026-04-27 07:53:50 -0700

    feat(creditmemo): add CreditMemo page class and tests for creating and crediting invoices

---
+++ tests/invoice/creditMemo.spec.ts
@@ -0,0 +1,65 @@
+import { INVOICE_DATA } from '../constants/invoiceData';
+import { ROLES } from '../constants/roles';
+import { test } from '../fixtures/baseFixture';
+import { CreditMemo } from '../pages/CreditMemo';
+import { InvoiceRecord } from '../pages/InvoiceRecord';
+import { today } from '../utils/dateUtils';
+import { generateMemo, generatePoNumber } from '../utils/nameGenerators';
+
+test.describe.serial('billinge responsible credits an invoice', () => {
+  let invoiceId: string;
+  let invoiceNumber: string;
+  let memo: string;
+
+  test('billing responsible can create a new Invoice @smoke', async ({ isolatedPage }) => {
+    // Arrange
+    const invoiceRecord = new InvoiceRecord(isolatedPage);
+    const poNumber = generatePoNumber();
+    const memo = generateMemo();
+
+    await invoiceRecord.switchRole(ROLES.egBillingResponsible);
+    await invoiceRecord.navigateToInvoice();
+
+    // Act
+    await invoiceRecord.setCustomer(INVOICE_DATA.customerText);
+    await invoiceRecord.verifySubsidiaryPrepopulated(INVOICE_DATA.subsidiaryId);
+    await invoiceRecord.setAccount(INVOICE_DATA.accountId);
+    await invoiceRecord.setMemo(memo);
+    await invoiceRecord.setPONumber(poNumber);
+    await invoiceRecord.setOrderedBy(INVOICE_DATA.orderedById);
+    await invoiceRecord.addLineItem(INVOICE_DATA.lineItemText);
+    await invoiceRecord.setLineItemDescription(INVOICE_DATA.lineItemDescription);
+    await invoiceRecord.setLineItemQuantity(INVOICE_DATA.lineItemQuantity);
+    await invoiceRecord.setLineItemRate(INVOICE_DATA.lineItemRate);
+    await invoiceRecord.setLineItemMainProduct(INVOICE_DATA.lineItemMainProductId);
+    await invoiceRecord.setLineItemSubProduct(INVOICE_DATA.lineItemSubProductId);
+    await invoiceRecord.setLineItemProductItem(INVOICE_DATA.lineItemProductItemId);
+    await invoiceRecord.setLineItemRevenueCategory(INVOICE_DATA.lineItemRevenueCategoryId);
+    await invoiceRecord.setLineItemDepartment(INVOICE_DATA.lineItemDepartmentId);
+    await invoiceRecord.setLineItemRevStartDate(today());
+    await invoiceRecord.setLineItemRevEndDate(today());
+    await invoiceRecord.addItem();
+    await invoiceRecord.save();
+
+    // Assert
+    await invoiceRecord.verifyRecordCreated();
+    invoiceId = new URL(isolatedPage.url()).searchParams.get('id')!;
+    invoiceNumber = await invoiceRecord.getInvoiceNumber();
+  });
+
+  test('billing responsible can credit an invoice', async ({ isolatedPage }) => {
+    // Arrange
+    const creditMemo = new CreditMemo(isolatedPage);
+    await creditMemo.switchRole(ROLES.egBillingResponsible);
+    await creditMemo.navigateTo(`/app/accounting/transactions/custinvc.nl?id=${invoiceId}`);
+
+    // Act
+    await creditMemo.clickCreditButton();
+    await creditMemo.verifyCreatedFromPrepopulated(invoiceNumber);
+    await creditMemo.save();
+
+    // Assert
+    await creditMemo.verifyRecordCreated();
+    await creditMemo.verifyCreatedFrom(invoiceNumber);
+  });
+});
---
+++ tests/pages/CreditMemo.ts
@@ -0,0 +1,38 @@
+import { expect, Page } from '@playwright/test';
+import { BasePage } from './BasePage';
+
+export class CreditMemo extends BasePage {
+  constructor(page: Page) {
+    super(page);
+  }
+
+  override async save(): Promise<void> {
+    await this.ensureFormInited();
+    await this.page.waitForLoadState('networkidle');
+    await this.page.waitForSelector('.ns-loading', { state: 'hidden' }).catch(() => {});
+    const saveBtn = this.page.locator('[id="btn_multibutton_submitter"]');
+    await saveBtn.waitFor({ state: 'visible' });
+    await saveBtn.click({ force: true });
+    // The create-from-transform URL already contains id= (e.g. ?memdoc=0&transform=custinvc&e=T&id=...)
+    // so we wait for the post-save URL where id= is the first query param.
+    await this.page.waitForURL(/custcred\.nl\?id=\d+/, { timeout: 60000 });
+    await this.page.waitForSelector('.ns-loading', { state: 'hidden' }).catch(() => {});
+  }
+
+  async clickCreditButton(): Promise<void> {
+    const creditBtn = this.page.locator('[id="credit"]');
+    await creditBtn.waitFor({ state: 'visible' });
+    await creditBtn.click();
+    await this.waitForNetSuiteLoad();
+  }
+
+  async verifyCreatedFromPrepopulated(invoiceNumber: string): Promise<void> {
+    await this.verifyFieldText('createdfrom', `Invoice #${invoiceNumber}`);
+  }
+
+  async verifyCreatedFrom(invoiceNumber: string): Promise<void> {
+    await expect(
+      this.page.locator('[data-field-name="createdfrom"] [data-nsps-type="field_input"]'),
+    ).toHaveText(`Invoice #${invoiceNumber}`);
+  }
+}
